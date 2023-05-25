6e0b57f40d8f8cb8274ddc4206b18c68ee7ac6f4
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-05-25 16:21:25 +0800

    fido uppercase

--- src/Homesrc/Homeinfo.jsx
+++ src/Homesrc/Homeinfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAddress} from "@thirdweb-dev/react";
+import { useAddress } from "@thirdweb-dev/react";
 import { Flex, Box, Text, Spacer, Heading, SimpleGrid, Image, Link, Center, Button, Input } from '@chakra-ui/react'
 import Seller from '../assets/social-media-icons/seller.jpeg'
 import Buyer from '../assets/social-media-icons/buyer.jpeg'
@@ -25,11 +25,76 @@ const MainMint = () => {
           </div>
         </Box>
         <Box>
-            <Heading>Alias:</Heading>
-            <Input type="text" id="alias" placeholder="wallet address" />
-            <Button id="passwordless-register" onClick={handleRegisterClick} color={'black'}>Register</Button>
-            <Button id="passwordless-signin" onClick={handleSigninClick} color={'black'}>Login</Button>
-            {/* <Flex align="center" justify="center">
+          <Heading>Alias:</Heading>
+          <Input type="text" id="alias" placeholder="wallet address" />
+          <Button
+            onClick={async function handleRegisterClick(e) {
+              e.preventDefault();
+
+              const alias = document.getElementById("alias").value;
+              // Status("Starting registering...");
+
+              const p = new Passwordless.Client({
+                apiKey: API_KEY
+              });
+
+              const backendRequest = await fetch(
+                BACKEND_URL + "/create-token?alias=" + alias
+              );
+              const backendResponse = await backendRequest.json();
+              if (!backendRequest.ok) {
+                console.log("Our backend failed while creating a token: ");
+                return;
+              }
+              const { token, error } = await p.register(backendResponse.token);
+            }}
+            color={'black'}
+          >
+            Register
+          </Button>
+          <Button
+            onClick={async function handleSigninClick(e) {
+              e.preventDefault();
+              const alias = document.getElementById("alias").value;
+
+
+              /**
+               * Initiate the Passwordless client with your public api key
+               */
+              const p = new Passwordless.Client({
+                apiKey: API_KEY,
+              });
+
+              try {
+                /**
+                 * Sign in - The Passwordless API and the browser initiates a sign in based on the alias
+                 */
+
+                //var userId = await fetch("user/id").then(r => r.json()); // get user id from database
+
+                const { token, error } = await p.signinWithAlias(alias);
+                //const token = await p.signinWithId(486761564);
+
+                console.log("Received token", token);
+                /**
+                 * Verify the sign in - Call your node backend to verify the token created from the sign in
+                 */
+                const user = await fetch(BACKEND_URL + "/verify-signin?token=" + token).then((r) => r.json());
+
+                /**
+                 * Done - you can now check the user result for status, userid etc
+                 */
+
+                console.log("User", user);
+              } catch (e) {
+                console.error("Things went really bad: ", e);
+              }
+            }}
+            color={'black'}
+          >
+            Login
+          </Button>
+          {/* <Flex align="center" justify="center">
             <Text mb='8px'>Alias:</Text>
             <Input
               width="250px"
